4ce72a401840f6f82131447dbb9183f7f7f7996f
powerpp-art <84716951+powerpp-art@users.noreply.github.com>
2021-05-25 20:39:03 +0900

    Add files via upload

---
+++ precache-manifest.7f97fa3d661dbb1f4fc600ddf8383504.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906085bf68107556228ec5aef629aba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2a537fe07b65ad9b63b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f1b92fbc1420c7558e97",
+    "url": "/static/css/main.dbafd27f.chunk.css"
+  },
+  {
+    "revision": "b2a537fe07b65ad9b63b",
+    "url": "/static/js/2.81d815a8.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.81d815a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b92fbc1420c7558e97",
+    "url": "/static/js/main.c91fba2b.chunk.js"
+  },
+  {
+    "revision": "84d71fa97e8ebc9089af",
+    "url": "/static/js/runtime-main.ae1255ed.js"
+  },
+  {
+    "revision": "37315df014e77e941b5de487f8a0fce4",
+    "url": "/static/media/inter.37315df0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
